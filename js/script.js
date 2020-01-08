8a73fb365c1e18dcb186138465e456972515574c
MichaelC2 <Michaelgcoker2@gmail.com>
2020-01-08 23:23:45 +0000

    Tic Tac Toe 2020

--- js/script.js
+++ js/script.js
@@ -1,43 +1,75 @@
+let Currentletter = "O" 
+function getTile() {
+    return "<h4>" + Currentletter + "</h4>"
+}
+function toggleLetter() {
+    if (Currentletter === "O") {
+        Currentletter = "X"
+    }
+    if (Currentletter === "X") {
+        Currentletter = "O"
+    }
+}
+
+var gameTerminated = false;
+var player = "O";
 
 
 
-
-
-
+function performLogict(button, title){
+    if(!gameTerminated){
+        $(button).hide()
+        $(title).html(player)
+        changePlayer();
+        checkHorizon();
+        checkVertical();
+        checkDiagnal();
+        tieGame();
+    }        
+}
 
 $("#button1").click(function() {
-    performLogic("#button1","#tile1");
+   $("#button1").replaceWith(getTile())
+   toggleLetter()
 });
 
 $("#button2").click(function() {
-    performLogic("#button2","#tile2");
+  $("#button2").replaceWith(getTile())
+  toggleLetter()
 });
 
 $("#button3").click(function() {
-    performLogic("#button3","#tile3");
+    $("#button3").replaceWith(getTile())
+    toggleLetter()
 });
 
 $("#button4").click(function() {
-    performLogic("#button4","#tile4");
+ $("#button4").replaceWith(getTile())
+ toggleLetter()
 });
 
 $("#button5").click(function() {
-    performLogic("#button5","#tile5");
+   $("#button5").replaceWith(getTile())
+   toggleLetter()
 });
 
 $("#button6").click(function() {
-    performLogic("#button6","#tile6");
+   $("#button6").replaceWith(getTile())
+   toggleLetter()
 });
 
 $("#button7").click(function() {
-    performLogic("#button7","#tile7");
+    $("#button7").replaceWith(getTile())
+    toggleLetter()
 });
 
 $("#button8").click(function() {
-    performLogic("#button8","#tile8");
+   $("#button8").replacetWith(getTile())
+    toggleLetter()
 });
-
+``
 $("#button9").click(function() {
-    performLogic("#button9","#tile9");
+    $("#button9").replaceWith(getTile())
+    toggleLetter()
 });
 
